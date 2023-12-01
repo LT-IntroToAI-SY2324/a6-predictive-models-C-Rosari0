585186438b0dfea9520616ed984e9391479d9c73
Cristian <cnrosario@cps.edu>
2023-12-01 11:19:41 -0600

    Finished a6_part2

---
+++ part2-training-testing-data/diabetes.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_diabetes
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+data = load_diabetes 
