6f28bd05b9fdd02bde6e4e4c73f4b5a5c7c24167
Cristian <cnrosario@cps.edu>
2023-12-04 10:53:43 -0600

    Just pushing to git hub

--- part2-training-testing-data/diabetes.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.datasets import load_diabetes
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-data = load_diabetes 
